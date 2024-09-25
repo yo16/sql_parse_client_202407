66d51ab2e980b4965ffdc6f34990f9314952ab7b
yo16 <xxyo1xx@gmail.com>
2024-09-25 13:15:30 +0900

    remove unused sources

--- sql_visualizer/src/UIComponents/CanvasComponents/constCanvasComponents.ts
+++ sql_visualizer/src/UIComponents/CanvasComponents/constCanvasComponents.ts
@@ -24,22 +24,12 @@ export const FROM_WIDTH = 100;
 // from句のテーブル名の高さ
 export const FORM_NAME_HEIGHT = 30;
 
-// columns要素内の隙間
-export const INCOLUMNS_ITEMS_PADDING = 2;   // 削除予定
 // columnの幅
 export const COLUMN_WIDTH = 100;
-// columnの列名を書く部分の高さ
-export const COLUMN_NAME_HEIGHT = 30;   // 削除予定
 
-// columnElementsBoxとcolumnElementの隙間
-export const COLELMS_PADDING = 4;   // 削除予定
 // Element要素間の隙間
 export const ELM_ITEMS_PADDING = 4;
 
-// columnElementの高さ
-export const COLELM_HEIGHT = 24;    //削除予定
-// columnElementのインデント
-export const COLELM_INDENT_WIDTH = 20;  // 削除予定
 export const ELM_INDENT_WIDTH = 20;
 // Elementの高さ（フォントに合わせるため仮の値）
 export const ELM_HEIGHT = 24;
